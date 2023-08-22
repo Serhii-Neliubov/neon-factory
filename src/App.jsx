920bf1c9a028a22b92c081a50ba2bd7a29545c0c
Serhii Neliubov <sergeyneliubov@gmail.com>
2023-08-22 13:33:45 +0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import "./App.css";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import html2canvas from "html2canvas";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoibmVvbi1mYWN0b3J5IiwiYSI6ImNrcWlpZzk1MzJvNWUyb3F0Z2UzaWZ5emQifQ.T-AqPH9OSIcwSLxebbyh8A";
@@ -15,9 +16,7 @@ function App() {
   const districtButtons = document.querySelectorAll(".toggleButton");
   const submenu = document.querySelector(".submenu");
   const controlsButton = document.querySelector("#controlsButton");
-  const centralisedButton = document.getElementById("CBDEULouiseNorthButton");
   const sidebarLabels = document.querySelectorAll(".sidebar-label");
-  const allDistrictsButton = document.getElementById("allDistrictsButton");
 
   const [districts, setDistricts] = useState({
     CBD: true,
@@ -94,56 +93,6 @@ function App() {
     map.setPitch(currentPitch - 10);
   }
 
-  function resetMapButtonHandler() {
-    map.flyTo({
-      center: [4.387564, 50.838193],
-      zoom: 11,
-      bearing: 0,
-      pitch: 0,
-    });
-
-    // Reset the visibility of all districts
-    for (let district in districts) {
-      toggleDistrictLayerVisibility(district, districts[district]);
-    }
-
-    // Reset the visibility of the main layer
-    map.setLayoutProperty("districts-brussels-0-2", "visibility", "visible");
-
-    allDistrictsButton.addEventListener("click", function () {
-      // Toggle the visibility of the "districts-brussels-0-2" layer
-      toggleDistrictLayerVisibility();
-      // Toggle the text of the button
-      toggleButtonText(allDistrictsButton, "All Districts", true);
-    });
-
-    // Reset button text
-    toggleButtonText(allDistrictsButton, "All Districts", true);
-    toggleButtonText(centralisedButton, "Show Centralised Districts", false);
-
-    districtButtons.forEach((button) => {
-      toggleButtonText(button, button.getAttribute("data-district"), false);
-    });
-
-    // Reset the sidebar label visibility
-
-    sidebarLabels.forEach((label) => {
-      label.classList.remove("hidden");
-    });
-  }
-
-  districtButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      var district = button.getAttribute("data-district");
-
-      // Toggle the visibility of the sidebar label item named "North"
-      toggleSidebarLabelItemVisibility(district);
-
-      // Toggle the visibility of the "districts-brussels-0-2" layer based on 'sidebar_label' property
-      toggleDistrictLayerVisibility(district);
-    });
-  });
-
   function toggleDistrictLayerVisibility(district, isVisible) {
     var layerId = "districts-brussels-0-2";
     var visibility = map.getLayoutProperty(layerId, "visibility");
@@ -151,6 +100,8 @@ function App() {
     if (!isVisible) {
       map.setLayoutProperty(layerId, "visibility", "visible");
     }
+
+    var updatedDistricts = { ...districts };
 
     if (district === "Centralised") {
       // Toggle the visibility of the "districts-brussels-0-2" layer for CBD, EU, Louise, and North
@@ -182,7 +133,8 @@ function App() {
     }
 
     // Update the visibility of the district
-    districts[district] = isVisible;
+    updatedDistricts[district] = isVisible;
+    setDistricts(updatedDistricts);
 
     // Update the filter to show/hide the district
     var filter = ["==", ["get", "sidebar_label"], district];
@@ -190,11 +142,96 @@ function App() {
     map.setLayoutProperty(layerId, "visibility", visibility);
   }
 
+  function allDistrictsButtonHandler() {
+    districts.forEach((district) => {
+      toggleDistrictLayerVisibility(district, true);
+    });
+  }
+
+  function printScreenHandler() {
+    const center = map.getCenter();
+    const currentZoom = map.getZoom();
+
+    // Создайте временный элемент, чтобы вставить карту для скриншота
+    const tempDiv = document.createElement("div");
+    tempDiv.style.width = "1920px"; // Ширина скриншота
+    tempDiv.style.height = "1080px"; // Высота скриншота
+    tempDiv.style.position = "absolute";
+    tempDiv.style.top = "0";
+    tempDiv.style.left = "-99999px"; // Делаем элемент невидимым
+    document.body.appendChild(tempDiv);
+
+    // Создайте новую карту во временном элементе
+    const tempMap = new mapboxgl.Map({
+      container: tempDiv,
+      style: "mapbox://styles/neon-factory/clf8a2hoq001r01mukt9dgsmp",
+      center: center,
+      zoom: currentZoom,
+    });
+
+    // Дождитесь загрузки карты
+    tempMap.on("load", () => {
+      // Используйте html2canvas для создания скриншота
+      html2canvas(tempMap.getCanvas()).then(function (canvas) {
+        // Преобразуйте скриншот в изображение
+        const imgData = canvas.toDataURL("image/png");
+
+        // Создайте ссылку для скачивания изображения
+        const a = document.createElement("a");
+        a.href = imgData;
+        a.download = "map-screenshot.png";
+
+        // Добавьте ссылку на страницу и выполните клик для скачивания
+        document.body.appendChild(a);
+        a.click();
+
+        // Очистите ресурсы и удалите временный элемент
+        document.body.removeChild(a);
+        tempMap.remove();
+        document.body.removeChild(tempDiv);
+      });
+    });
+  }
+
+  function resetMapButtonHandler() {
+    map.flyTo({
+      center: [4.387564, 50.838193],
+      zoom: 11,
+      bearing: 0,
+      pitch: 0,
+    });
+
+    // Reset the visibility of all districts
+    for (let district in districts) {
+      toggleDistrictLayerVisibility(district, districts[district]);
+    }
+
+    // Reset the visibility of the main layer
+    map.setLayoutProperty("districts-brussels-0-2", "visibility", "visible");
+
+    // Reset the sidebar label visibility
+
+    sidebarLabels.forEach((label) => {
+      label.classList.remove("hidden");
+    });
+  }
+
+  districtButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      var district = button.getAttribute("data-district");
+
+      // Toggle the visibility of the sidebar label item named "North"
+      toggleSidebarLabelItemVisibility(district);
+
+      // Toggle the visibility of the "districts-brussels-0-2" layer based on 'sidebar_label' property
+      toggleDistrictLayerVisibility(district);
+    });
+  });
+
   function centralisedButtonHandler() {
     var districtsToShow = ["CBD", "EU", "Louise", "North", "South"];
 
     toggleDistrictLayerVisibility(districtsToShow);
-    toggleButtonText(centralisedButton);
   }
 
   function toggleSidebarLabelItemVisibility(district) {
@@ -206,23 +243,6 @@ function App() {
     if (sidebarLabelItem) {
       // Toggle the visibility of the sidebar label item
       sidebarLabelItem.classList.toggle("hidden");
-    }
-  }
-
-  function toggleButtonText(button, districtName, isAllDistricts) {
-    var buttonText = button.textContent;
-    if (buttonText.includes("Hide")) {
-      if (isAllDistricts) {
-        button.textContent = "Show All Districts";
-      } else {
-        button.textContent = "Show " + districtName;
-      }
-    } else {
-      if (isAllDistricts) {
-        button.textContent = "Hide All Districts";
-      } else {
-        button.textContent = "Hide " + districtName;
-      }
     }
   }
 
@@ -247,15 +267,7 @@ function App() {
   return (
     <div id="mapContainer">
       <div className="sidebar">
-        <div className="sidebarCompany">
-          <img
-            style={{ width: "25px" }}
-            alt="Logo"
-            className="logo"
-            src="logo.png"
-          />
-          <h1 className="">Neon Factory</h1>
-        </div>
+        <img alt="Logo" className="logo" src="LOGO_Neon-Factory.png" />
         <div id="geocoderContainer"></div>
         <div className="greenLine"></div>
         <button
@@ -315,7 +327,11 @@ function App() {
         Brussels
         <div className="greenLine"></div>
         <div className="toggleContainer">
-          <button className="toggleButton" id="allDistrictsButton">
+          <button
+            onClick={() => allDistrictsButtonHandler()}
+            className="toggleButton"
+            id="allDistrictsButton"
+          >
             All Districts
           </button>
           <button
@@ -358,21 +374,30 @@ function App() {
           >
             South
           </button>
-          <button className="toggleButton" data-district="SE" id="SEButton">
-            South-East
-          </button>
-          <button className="toggleButton" data-district="SW" id="SWButton">
-            South-West
+          <button
+            onClick={() => setDistricts(!districts.SouthEst)}
+            className="toggleButton"
+            data-district="SE"
+            id="SEButton"
+          >
+            {districts.SouthEst ? "Show SouthEst" : "Hide SouthEst"}
           </button>
           <button
             className="toggleButton"
-            onClick={() => setDistricts(!districts.Airport)}
+            data-district="SW"
+            id="SWButton"
+          ></button>
+          <button
+            className="toggleButton"
             data-district="Airport"
             id="AirportButton"
           >
             {districts.Airport ? "Show Airport" : "Hide Airport"}
           </button>
           <div className="greenLine"></div>
+          <button onClick={printScreenHandler} className="PrintScreen">
+            Print Screen
+          </button>
         </div>
       </div>
       <div id="map" style={{ flex: 1 }}></div>
