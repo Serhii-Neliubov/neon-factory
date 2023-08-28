59dd78e6d0db796ce68c6061e7abb0e6c189863a
Serhii Neliubov <sergeyneliubov@gmail.com>
2023-08-28 16:38:33 +0300

    add

--- src/utils/DefaultDrawStyles.js
+++ src/utils/DefaultDrawStyles.js
@@ -1,34 +1,29 @@
 const defaultDrawStyles = [
   {
-    id: "gl-draw-line",
-    type: "line",
+    id: "gl-draw-polygon-fill-inactive",
+    type: "fill",
     filter: [
       "all",
-      ["==", "$type", "LineString"],
+      ["==", "active", "false"],
+      ["==", "$type", "Polygon"],
       ["!=", "mode", "static"],
     ],
-    layout: {
-      "line-cap": "round",
-      "line-join": "round",
-    },
     paint: {
-      "line-color": "#000",
-      "line-dasharray": [0.2, 2],
-      "line-width": 2,
-    },
-  },
-  // polygon fill
-  {
-    id: "gl-draw-polygon-fill",
-    type: "fill",
-    filter: ["all", ["==", "$type", "Polygon"], ["!=", "mode", "static"]],
-    paint: {
-      "fill-color": "#000",
-      "fill-outline-color": "#D20C0C",
+      "fill-color": "#3bb2d0",
+      "fill-outline-color": "#3bb2d0",
       "fill-opacity": 0.1,
     },
   },
-  // polygon mid points
+  {
+    id: "gl-draw-polygon-fill-active",
+    type: "fill",
+    filter: ["all", ["==", "active", "true"], ["==", "$type", "Polygon"]],
+    paint: {
+      "fill-color": "#fbb03b",
+      "fill-outline-color": "#fbb03b",
+      "fill-opacity": 0.1,
+    },
+  },
   {
     id: "gl-draw-polygon-midpoint",
     type: "circle",
@@ -38,25 +33,72 @@ const defaultDrawStyles = [
       "circle-color": "#fbb03b",
     },
   },
-  // polygon outline stroke
-  // This doesn't style the first edge of the polygon, which uses the line stroke styling instead
   {
-    id: "gl-draw-polygon-stroke-active",
+    id: "gl-draw-polygon-stroke-inactive",
     type: "line",
-    filter: ["all", ["==", "$type", "Polygon"], ["!=", "mode", "static"]],
+    filter: [
+      "all",
+      ["==", "active", "false"],
+      ["==", "$type", "Polygon"],
+      ["!=", "mode", "static"],
+    ],
     layout: {
       "line-cap": "round",
       "line-join": "round",
     },
     paint: {
-      "line-color": "#000",
+      "line-color": "#3bb2d0",
+      "line-width": 2,
+    },
+  },
+  {
+    id: "gl-draw-polygon-stroke-active",
+    type: "line",
+    filter: ["all", ["==", "active", "true"], ["==", "$type", "Polygon"]],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": "#fbb03b",
       "line-dasharray": [0.2, 2],
       "line-width": 2,
     },
   },
-  // vertex point halos
   {
-    id: "gl-draw-polygon-and-line-vertex-halo-active",
+    id: "gl-draw-line-inactive",
+    type: "line",
+    filter: [
+      "all",
+      ["==", "active", "false"],
+      ["==", "$type", "LineString"],
+      ["!=", "mode", "static"],
+    ],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": "#3bb2d0",
+      "line-width": 2,
+    },
+  },
+  {
+    id: "gl-draw-line-active",
+    type: "line",
+    filter: ["all", ["==", "$type", "LineString"], ["==", "active", "true"]],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": "#fbb03b",
+      "line-dasharray": [0.2, 2],
+      "line-width": 2,
+    },
+  },
+  {
+    id: "gl-draw-polygon-and-line-vertex-stroke-inactive",
     type: "circle",
     filter: [
       "all",
@@ -66,12 +108,11 @@ const defaultDrawStyles = [
     ],
     paint: {
       "circle-radius": 5,
-      "circle-color": "#000",
+      "circle-color": "#fff",
     },
   },
-  // vertex points
   {
-    id: "gl-draw-polygon-and-line-vertex-active",
+    id: "gl-draw-polygon-and-line-vertex-inactive",
     type: "circle",
     filter: [
       "all",
@@ -81,52 +122,111 @@ const defaultDrawStyles = [
     ],
     paint: {
       "circle-radius": 3,
-      "circle-color": "#000",
+      "circle-color": "#fbb03b",
     },
   },
-
-  // INACTIVE (static, already drawn)
-  // line stroke
   {
-    id: "gl-draw-line-static",
-    type: "line",
+    id: "gl-draw-point-point-stroke-inactive",
+    type: "circle",
     filter: [
       "all",
-      ["==", "$type", "LineString"],
-      ["==", "mode", "static"],
+      ["==", "active", "false"],
+      ["==", "$type", "Point"],
+      ["==", "meta", "feature"],
+      ["!=", "mode", "static"],
     ],
-    layout: {
-      "line-cap": "round",
-      "line-join": "round",
-    },
     paint: {
-      "line-color": "#000",
-      "line-width": 3,
+      "circle-radius": 5,
+      "circle-opacity": 1,
+      "circle-color": "#fff",
     },
   },
-  // polygon fill
+  {
+    id: "gl-draw-point-inactive",
+    type: "circle",
+    filter: [
+      "all",
+      ["==", "active", "false"],
+      ["==", "$type", "Point"],
+      ["==", "meta", "feature"],
+      ["!=", "mode", "static"],
+    ],
+    paint: {
+      "circle-radius": 3,
+      "circle-color": "#3bb2d0",
+    },
+  },
+  {
+    id: "gl-draw-point-stroke-active",
+    type: "circle",
+    filter: [
+      "all",
+      ["==", "$type", "Point"],
+      ["==", "active", "true"],
+      ["!=", "meta", "midpoint"],
+    ],
+    paint: {
+      "circle-radius": 7,
+      "circle-color": "#fff",
+    },
+  },
+  {
+    id: "gl-draw-point-active",
+    type: "circle",
+    filter: [
+      "all",
+      ["==", "$type", "Point"],
+      ["!=", "meta", "midpoint"],
+      ["==", "active", "true"],
+    ],
+    paint: {
+      "circle-radius": 5,
+      "circle-color": "#fbb03b",
+    },
+  },
   {
     id: "gl-draw-polygon-fill-static",
     type: "fill",
-    filter: ["all", ["==", "$type", "Polygon"], ["==", "mode", "static"]],
+    filter: ["all", ["==", "mode", "static"], ["==", "$type", "Polygon"]],
     paint: {
-      "fill-color": "#000",
-      "fill-outline-color": "#000",
+      "fill-color": "#404040",
+      "fill-outline-color": "#404040",
       "fill-opacity": 0.1,
     },
   },
-  // polygon outline
   {
     id: "gl-draw-polygon-stroke-static",
     type: "line",
-    filter: ["all", ["==", "$type", "Polygon"], ["==", "mode", "static"]],
+    filter: ["all", ["==", "mode", "static"], ["==", "$type", "Polygon"]],
     layout: {
       "line-cap": "round",
       "line-join": "round",
     },
     paint: {
-      "line-color": "#000",
-      "line-width": 3,
+      "line-color": "#404040",
+      "line-width": 2,
+    },
+  },
+  {
+    id: "gl-draw-line-static",
+    type: "line",
+    filter: ["all", ["==", "mode", "static"], ["==", "$type", "LineString"]],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": "#404040",
+      "line-width": 2,
+    },
+  },
+  {
+    id: "gl-draw-point-static",
+    type: "circle",
+    filter: ["all", ["==", "mode", "static"], ["==", "$type", "Point"]],
+    paint: {
+      "circle-radius": 5,
+      "circle-color": "#404040",
     },
   },
   {
@@ -164,11 +264,7 @@ const defaultDrawStyles = [
   {
     id: "gl-draw-polygon-color-picker",
     type: "fill",
-    filter: [
-      "all",
-      ["==", "$type", "Polygon"],
-      ["has", "user_portColor"],
-    ],
+    filter: ["all", ["==", "$type", "Polygon"], ["has", "user_portColor"]],
     paint: {
       "fill-color": ["get", "user_portColor"],
       "fill-outline-color": ["get", "user_portColor"],
@@ -178,11 +274,7 @@ const defaultDrawStyles = [
   {
     id: "gl-draw-line-color-picker",
     type: "line",
-    filter: [
-      "all",
-      ["==", "$type", "LineString"],
-      ["has", "user_portColor"],
-    ],
+    filter: ["all", ["==", "$type", "LineString"], ["has", "user_portColor"]],
     paint: {
       "line-color": ["get", "user_portColor"],
       "line-width": 2,
@@ -197,5 +289,5 @@ const defaultDrawStyles = [
       "circle-color": ["get", "user_portColor"],
     },
   },
-]
+];
 export default defaultDrawStyles;
