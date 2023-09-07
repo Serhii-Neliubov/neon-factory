36d384d84a1cffef6bfa83398405e60b49e820ac
Serhii Neliubov <sergeyneliubov@gmail.com>
2023-09-07 18:14:25 +0300

    some fixes

--- src/components/SubMenu/ActionControls.tsx
+++ src/components/SubMenu/ActionControls.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-export const ActionControls = ({
-  isControlsActive,
-  controlsButtonHandler,
-  setIsControlsActive,
-}) => {
+export const ActionControls = ({ isControlsActive, setIsControlsActive }) => {
   return (
     <button
       onClick={() => setIsControlsActive(!isControlsActive)}
-      className={`BrusselsButton`}
+      className={`BrusselsButton ${
+        isControlsActive ? "BrusselsButton_open" : ""
+      }`}
       id="controlsButton"
     >
       {isControlsActive ? "Close Controls" : "Open Controls"}
