160d7a24102d2c30d15fb4b208d5354af96ed659
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-10-10 11:34:59 +0200

    new func

--- src/App.jsx
+++ src/App.jsx
@@ -245,31 +245,6 @@ function App() {
       map.setLayoutProperty("poi-label", "visibility", "none");
     });
 
-    var myCircle = new MapboxCircle({ lat: 50.845193, lng: 4.387564 }, 25000, {
-      editable: true,
-      minRadius: 1500,
-      fillColor: "#29AB87",
-    }).addTo(map);
-
-    myCircle.on("centerchanged", function (circleObj) {
-      console.log("New center:", circleObj.getCenter());
-    });
-    myCircle.once("radiuschanged", function (circleObj) {
-      console.log("New radius (once!):", circleObj.getRadius());
-    });
-    myCircle.on("click", function (mapMouseEvent) {
-      console.log("Click:", mapMouseEvent.point);
-    });
-    myCircle.on("contextmenu", function (mapMouseEvent) {
-      console.log("Right-click:", mapMouseEvent.lngLat);
-    });
-    map.on("click", "0", function (e) {
-      const clickedPolygon = e.features[0]; // Получаем информацию о кликнутом полигоне
-      // Вызываем функцию для отображения метров полигона
-
-      showPolygonArea(clickedPolygon);
-    });
-
     map.on("click", function (e) {
       if (!newDrawFeature.current) {
         var drawFeatureAtPoint = draw.getFeatureIdsAt(e.point);
@@ -289,6 +264,12 @@ function App() {
       }
 
       newDrawFeature.current = false;
+    });
+    map.on("click", "0", function (e) {
+      const clickedPolygon = e.features[0]; // Получаем информацию о кликнутом полигоне
+      // Вызываем функцию для отображения метров полигона
+
+      showPolygonArea(clickedPolygon);
     });
     colorPicker.current.addEventListener("change", function (event) {
       changeColor(event.target.value, mapboxgl, draw);
@@ -549,7 +530,54 @@ function App() {
   //   setIsModalActive(!isModalActive);
   //   defaultStyleHandler();
   // }
-  function createCircleButton() {}
+  function generateUniqueId() {
+    return "_" + Math.random().toString(36).substr(2, 9);
+  }
+  const circles = [];
+  console.log(circles);
+  function createCircleButton() {
+    if (map) {
+      const uniqueId = generateUniqueId();
+      var myCircle = new MapboxCircle({ lat: 50.845193, lng: 4.387564 }, 3275, {
+        editable: true,
+        minRadius: 10,
+        fillColor: "#29AB87",
+        id: uniqueId, // Добавьте уникальный идентификатор круга
+      }).addTo(map);
+
+      circles.push(myCircle);
+
+      const radiusDisplay = document.getElementById("radius-display");
+      let radius = 3275;
+      radiusDisplay.textContent = `Radius: ${radius} meters`;
+      myCircle.on("centerchanged", function (circleObj) {
+        console.log("New center:", circleObj.getCenter());
+      });
+      myCircle.on("radiuschanged", function (circleObj) {
+        radius = circleObj.getRadius();
+        radiusDisplay.textContent = `Radius: ${radius} meters`; // Обновляем текст с радиусом
+      });
+      myCircle.on("click", function (mapMouseEvent) {
+        console.log("Click:", mapMouseEvent.point);
+      });
+      myCircle.on("contextmenu", function (mapMouseEvent) {
+        console.log("Right-click:", mapMouseEvent.lngLat);
+      });
+
+      const deleteCircleButton = document.getElementById(
+        "delete-circle-button"
+      );
+      // Добавляем обработчик события клика на кнопку
+      deleteCircleButton.addEventListener("click", function () {
+        if (circles.length > 0) {
+          // Удаляем последний созданный круг из массива
+          const myCircle = circles.pop();
+          myCircle.remove(); // Удаляем текущий круг
+          radiusDisplay.textContent = ""; // Очищаем текст с радиусом
+        }
+      });
+    }
+  }
 
   function centralisedDistrictsButtonHandler() {
     if (isCentralisedDistrictsVisible) {
@@ -1025,6 +1053,8 @@ function App() {
           onChange={(event) => changeColor(event.target.value, mapboxgl, draw)}
         />
         <img alt="Logo" className="logo-map" src="logo.png" />
+        <div id="radius-display"></div>
+        <button id="delete-circle-button">Удалить круг</button>
       </div>
     </div>
   );
