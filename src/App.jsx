79ff862bbf5d45ff66b4b031b7cdb56be23df499
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-10-10 16:10:26 +0200

    saved func

--- src/App.jsx
+++ src/App.jsx
@@ -161,7 +161,53 @@ function App() {
       label.classList.remove("hidden");
     });
   }
+  function generateUniqueId() {
+    return "_" + Math.random().toString(36).substr(2, 9);
+  }
+  const circles = [];
+  function createCircleButton() {
+    if (map) {
+      const uniqueId = generateUniqueId();
+      var myCircle = new MapboxCircle({ lat: 50.845193, lng: 4.387564 }, 3275, {
+        editable: true,
+        minRadius: 10,
+        fillColor: "#29AB87",
+        id: uniqueId, // Добавьте уникальный идентификатор круга
+      }).addTo(map);
 
+      circles.push(myCircle);
+
+      const radiusDisplay = document.getElementById("radius-display");
+      let radius = 3275;
+      radiusDisplay.textContent = `Radius: ${radius} meters`;
+      myCircle.on("centerchanged", function (circleObj) {
+        console.log("New center:", circleObj.getCenter());
+      });
+      myCircle.on("radiuschanged", function (circleObj) {
+        radius = circleObj.getRadius();
+        radiusDisplay.textContent = `Radius: ${radius} meters`; // Обновляем текст с радиусом
+      });
+      myCircle.on("click", function (mapMouseEvent) {
+        console.log("Click:", mapMouseEvent.point);
+      });
+      myCircle.on("contextmenu", function (mapMouseEvent) {
+        console.log("Right-click:", mapMouseEvent.lngLat);
+      });
+
+      const deleteCircleButton = document.getElementById(
+        "delete-circle-button"
+      );
+      // Добавляем обработчик события клика на кнопку
+      deleteCircleButton.addEventListener("click", function () {
+        if (circles.length > 0) {
+          // Удаляем последний созданный круг из массива
+          const myCircle = circles.pop();
+          myCircle.remove(); // Удаляем текущий круг
+          radiusDisplay.textContent = ""; // Очищаем текст с радиусом
+        }
+      });
+    }
+  }
   useEffect(() => {
     mapboxgl.accessToken =
       "pk.eyJ1IjoibmVvbi1mYWN0b3J5IiwiYSI6ImNrcWlpZzk1MzJvNWUyb3F0Z2UzaWZ5emQifQ.T-AqPH9OSIcwSLxebbyh8A";
@@ -999,6 +1045,9 @@ function App() {
           onChange={(event) => changeColor(event.target.value, mapboxgl, draw)}
         />
         <img alt="Logo" className="logo-map" src="logo.png" />
+        <button onClick={createCircleButton} id="createCircleButton">
+          Create a circle
+        </button>
       </div>
     </div>
   );
