78cd76da5011d0bd3922b9ed1006e1c822c8814e
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-10-11 02:14:08 +0200

    fixes

--- src/components/MapStyleButtons/DefaultStyle.tsx
+++ src/components/MapStyleButtons/DefaultStyle.tsx
@@ -2,37 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 
-const DefaultStyle = ({ mapStyleSetter }) => {
+const DefaultStyle = ({ mapStyleSetter, map, defaultStyleHandler }) => {
   const [toggleClass, setToggleClass] = useState<string>("");
   const [toggle, setToggle] = useState<boolean>(true);
-  const map = useSelector((state: RootState) => state.map.value);
-
-  function defaultStyleHandler() {
-    map.setStyle("mapbox://styles/neon-factory/clle3pwwc010r01pm1k5f605b");
-    setShowCadastre(false);
-    setMapStyleSetter(1);
-    setShowTransport(true);
-    setServicesAction(false);
-
-    const inputElement = document.querySelector(".DefaultInput");
-    if (inputElement) {
-      inputElement.checked = true;
-    }
-
-    if (map) {
-      map.loadImage("pin.png", function (error, image) {
-        if (error) throw error;
-        map.addImage("custom-pin", image);
-      });
-
-      // Остальной код обработки карты также может быть здесь
-    }
-    setSelectedDistricts([]);
-    setCentralisedToggle(false);
-    // setIsModalActive(false);
-    setDecentralisedToggle(false);
-    setAllDistrictsToggle(false);
-  }
 
   const handleClick = () => {
     setToggle(true);
@@ -47,8 +19,8 @@ const DefaultStyle = ({ mapStyleSetter }) => {
   return (
     <div
       onClick={() => {
-        defaultStyleHandler();
         handleClick();
+        defaultStyleHandler();
       }}
       className="toggleButton"
     >
