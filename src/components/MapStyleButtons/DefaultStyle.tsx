83fa58ac24860a45ebaa1291340cd584693f8ea5
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-10-11 10:46:22 +0200

    redux change map style

--- src/components/MapStyleButtons/DefaultStyle.tsx
+++ src/components/MapStyleButtons/DefaultStyle.tsx
@@ -1,11 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
+import { decentralisedToggleFalse } from "../../redux/slices/decentralisedToggleSlice";
+import { allDistrictsToggleFalse } from "../../redux/slices/allDistrictsToggleSlice";
+import { centralisedToggleFalse } from "../../redux/slices/centralisedToggleSlice";
+import { showCadastreFalse } from "../../redux/slices/showCadastreSlice";
+import { showTransportTrue } from "../../redux/slices/showTransportSlice";
+import { servicesActionFalse } from "../../redux/slices/servicesActionSlice";
 
-const DefaultStyle = ({ mapStyleSetter, map, defaultStyleHandler }) => {
+const DefaultStyle = ({
+  mapStyleSetter,
+  map,
+  setMapStyleSetter,
+  setSelectedDistricts,
+}) => {
   const [toggleClass, setToggleClass] = useState<string>("");
   const [toggle, setToggle] = useState<boolean>(true);
+  const dispatch = useDispatch();
 
+  function defaultStyleHandler() {
+    map.setStyle("mapbox://styles/neon-factory/clle3pwwc010r01pm1k5f605b");
+    dispatch(showCadastreFalse());
+    setMapStyleSetter(1);
+    dispatch(showTransportTrue());
+
+    dispatch(servicesActionFalse());
+
+    if (map) {
+      map.loadImage("pin.png", function (error, image) {
+        if (error) throw error;
+        map.addImage("custom-pin", image);
+      });
+
+      // Остальной код обработки карты также может быть здесь
+    }
+    setSelectedDistricts([]);
+    dispatch(centralisedToggleFalse());
+
+    // setIsModalActive(false);
+    dispatch(decentralisedToggleFalse());
+
+    dispatch(allDistrictsToggleFalse());
+  }
   const handleClick = () => {
     setToggle(true);
   };
