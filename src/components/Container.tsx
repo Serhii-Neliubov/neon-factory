6ca0e4baf6750d858a4651570f1d87085e02b96a
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-10-14 18:20:38 +0200

    new styles

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import styles from "./Container.module.scss";
 
 const Container = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+  return <div className="mapContainer">{children}</div>;
 };
 
 export default Container;
